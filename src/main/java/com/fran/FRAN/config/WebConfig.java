94edddebd618d84312ea74b08473f22c7896c87e
João Guilherme <joao.novaes@aluno.ifsp.edu.br>
2024-10-22 15:54:49 -0300

    👨‍💻Classes para configurar cors

---
+++ src/main/java/com/fran/FRAN/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.fran.FRAN.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+//isso aqui tudo é pro cors para de perturbar minha cabeça com "nao autorizado"
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // URL do frontend vue
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
