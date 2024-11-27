0db69b555d24e786fafc90fdad9a76ae1a6aecd1
João Guilherme <joao.novaes@aluno.ifsp.edu.br>
2024-11-27 13:18:41 -0300

    @Enumerated(EnumType.STRING)

--- src/main/java/com/fran/FRAN/dto/response/EstagioResponseDTO.java
+++ src/main/java/com/fran/FRAN/dto/response/EstagioResponseDTO.java
@@ -6,10 +6,11 @@ import com.fran.FRAN.model.entity.Aluno;
 import com.fran.FRAN.model.entity.Empresa;
 import com.fran.FRAN.model.entity.Estagio;
 import com.fran.FRAN.model.entity.Orientador;
+import com.fran.FRAN.model.entity.Status;
 
-public record EstagioResponseDTO(Aluno aluno, Orientador orientador, Empresa empresa, Integer cargaDiaria, LocalDate dataInicio, LocalDate dataTermino, String status, Boolean obrigatorio) {
+public record EstagioResponseDTO(Aluno aluno, Orientador orientador, Empresa empresa, Integer cargaDiaria, LocalDate dataInicio, LocalDate dataTermino, Status status, Boolean obrigatorio) {
     public EstagioResponseDTO(Estagio estagio){
         this(estagio.getAluno(), estagio.getOrientador(), estagio.getEmpresa(), estagio.getCargaDiaria(), estagio.getDataInicio(), estagio.getDataTermino(),
-        estagio.getStatus().toString(), estagio.getObrigatorio());
+        estagio.getStatus(), estagio.getObrigatorio());
     }
 }
--- src/main/java/com/fran/FRAN/model/entity/Estagio.java
+++ src/main/java/com/fran/FRAN/model/entity/Estagio.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -51,6 +53,7 @@ public class Estagio {
     private LocalDate dataTermino;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     // Relacionamento com Aluno
